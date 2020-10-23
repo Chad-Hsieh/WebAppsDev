1971b29975740d9daed61073ce518bbe8c57da56
Chad Hsieh <zowk52@gmail.com>
2020-10-23 15:36:42 +0000

    Add one-to-many relationship between Photo and Comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,6 @@
 class Comment < ApplicationRecord
+    #Comment has foreign key: photo_id 
+    belongs_to :photo #this line let the nodes know it belongs to a photo(model).
+    
+    validates :content, presence: true
 end
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,3 +1,5 @@
 class Photo < ApplicationRecord
+    has_many :comments 
+    
     validates :photo_url, presence: true #for validation
 end
