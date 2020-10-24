092493996760068e8fdafb94600d9fbb27df84b6
Chad Hsieh <zowk52@gmail.com>
2020-10-24 02:06:19 +0000

    Add edit, update, destroy RESTful methods.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,6 @@
 class CommentsController < ApplicationController
   before_action :load_photo
+  before_action :load_comment, except: [:index, :new, :create]
   
   def index
     @comments = @photo.comments
@@ -21,14 +22,32 @@ class CommentsController < ApplicationController
   
   
   def show
-    @comment = @photo.commnets.find params[:id]
   end
 
+  def edit
+  end
+  
+  def update
+    if @comment.update comment_params
+      redirect_to [@photo,@comment], notice: "Comment updated."
+    else
+      render :edit
+    end
+  end
+  
+  def destroy
+    @comment.destroy
+    redirect_to photo_comments_path(@photo)
+  end
   
   private
   
   def load_photo
     @photo = Photo.find params[:photo_id] #/photos/54/comments
+  end
+  
+  def load_comment
+    @comment = @photo.comments.find params[:id]
   end
   
   def comment_params
