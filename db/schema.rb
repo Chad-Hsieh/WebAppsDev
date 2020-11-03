94f0a483d584e5d4a64009f55236e1e23f6c5630
Chad Hsieh <zowk52@gmail.com>
2020-11-03 16:33:02 +0000

    Add user model and set validations to the user model.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_23_151257) do
+ActiveRecord::Schema.define(version: 2020_11_03_162021) do
 
   create_table "comments", force: :cascade do |t|
     t.text "content"
@@ -24,6 +24,14 @@ ActiveRecord::Schema.define(version: 2020_10_23_151257) do
     t.string "photo_url"
     t.string "description"
     t.string "hashtag"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
