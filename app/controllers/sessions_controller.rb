f29007b62352c75cd1a399be4657053c20cffd68
Chad Hsieh <zowk52@gmail.com>
2020-11-06 12:43:21 +0000

    Add user friendly features: please login notice, redirect_url

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,9 @@ class SessionsController < ApplicationController
     @user = User.where(email: params[:email]).first
     if @user && @user.authenticate(params[:password])
       login(@user)
-      redirect_to root_path, notice: "Logged In"
+      redirect_url = session[:protected_page] ? session[:protected_page] : root_path
+      session[:protected_page] = nil
+      redirect_to redirect_url, notice: "Logged In"
     else
       redirect_to login_path, alert: "Invalid email/password"
     end
