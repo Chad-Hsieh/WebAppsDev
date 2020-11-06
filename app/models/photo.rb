dece9e41ad1cf2c6f5e37f0360dec37b79a5f459
Chad Hsieh <zowk52@gmail.com>
2020-11-06 12:24:14 +0000

    Add relationship between User and Photos

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,5 +1,6 @@
 class Photo < ApplicationRecord
     has_many :comments 
+    belongs_to :user
     
     validates :photo_url, presence: true #for validation
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_03_162021) do
+ActiveRecord::Schema.define(version: 2020_11_06_121722) do
 
   create_table "comments", force: :cascade do |t|
     t.text "content"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2020_11_03_162021) do
     t.string "hashtag"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
