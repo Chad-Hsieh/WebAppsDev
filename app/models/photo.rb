ef0c38785ca46d05d85d1a772a04af9ca43901bf
Ubuntu <ubuntu@ip-172-31-9-163.us-west-1.compute.internal>
2020-11-23 16:46:38 +0000

    Show image and link to single photo post

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,5 +2,7 @@ class Photo < ApplicationRecord
     has_many :comments 
     belongs_to :user
     
+    mount_uploader :photo_url, FileUploader
+    
     validates :photo_url, presence: true #for validation
 end
